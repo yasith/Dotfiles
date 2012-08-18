0c46bfdedaf83e4f9553ece5ef55e8753955b127
Yasith Vidanaarachchi <yasith.vidanaarachchi@gmail.com>
2012-08-18 16:43:40 -0400

    Comment xmonad.hs, add gitignore

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,14 @@
+-- Xmonad configuration file
+-- after changing this file, run "xmonad --recompile" to check for errors
+-- to load the changes, press mod-q. mod is Alt by default
+
 import XMonad
 
 
 -- Expand on the default config, with few cusom values
 -- Use urxvt as the default terminal
-
+-- Use light grey (brushed metal color) as normal border
+-- Use orange as selected border, red is too noticeable
 main = xmonad $ defaultConfig
   { borderWidth        = 2
   , terminal           = "urxvt"
